6c673f41b6649f985a0b64c6dd78ef4b2a64063b
Maxeg13 <chibisaincrada@gmail.com>
2017-07-12 18:22:28 +0400

    setPin

--- main.c
+++ main.c
@@ -3,13 +3,25 @@
  
 #define F_CPU 4000000UL
 #include <util/delay.h>
+#define byte int8_t
+void setPin(byte i)
+{
+PORTB|=(1<<i);	// Turn ON LEDs
+}
+
+
+void rstPin(byte i)
+{
+PORTB&=~(1<<i);	// Turn ON LEDs
+} 
+ 
  
 int main()
 {
 uint8_t i=5;
 uint8_t j=200;
 uint8_t r=1;
-	DDRB = 0b00001000;		// Configure port C as output
+	DDRB = 0b00000011;		// Configure port C as output
 
 
  
@@ -26,10 +38,10 @@ if(i==2)
 r=1;
 
 
-		PORTB= 0x08;	// Turn ON LEDs
+		setPin(1);	// Turn ON LEDs
 		for(int t=0;t<i;t++)
 		_delay_us(100); // Wait 250ms
-		PORTB = 0x00;	// Turn OFF LEDs
+		rstPin(1);	// Turn OFF LEDs
 		for(int t=0;t<j;t++)
 		_delay_us(100);	// Wail 250ms
 	}
